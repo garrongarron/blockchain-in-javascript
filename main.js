65a0136617ace23eb1a49124d2333e327a2d4d85
Samu Garron DEV <samugarrondev@gmail.com>
2022-03-30 14:44:19 -0300

    Proof of work

--- main.js
+++ main.js
@@ -8,35 +8,47 @@ class Block {
         this.data = data
         this.previousHash = previousHash
         this.hash = this.calculateHash()
+        this.nonce = 0
     }
     calculateHash(data) {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(data)).toString()
+        return SHA256(this.index + this.previousHash
+            + this.timestamp + JSON.stringify(data) + this.nonce).toString()
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== "0".repeat(difficulty)) {
+            this.nonce++
+            this.hash = this.calculateHash()
+        }
+        console.log("Block mined: " + this.hash);
     }
 }
+
 class BlockChain {
     constructor() {
         this.chain = [this.createGenesisBlock()]
+        this.difficulty = 4
     }
-    createGenesisBlock(){
+    createGenesisBlock() {
         return new Block(0, '01/01/2022', 'Genesisblock', "NONE")
     }
-    getLatesBlock(){
-        return this.chain[this.chain.length-1]
+    getLatesBlock() {
+        return this.chain[this.chain.length - 1]
     }
-    addBlock(newBlock){
+    addBlock(newBlock) {
         newBlock.previousHash = this.getLatesBlock().hash
-        newBlock.hash = newBlock.calculateHash()
+        newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock)
     }
-    isChainVAlid(){
+    isChainVAlid() {
         for (let index = 1; index < this.chain.length; index++) {
             const currentBlock = this.chain[index];
-            const previousBlock = this.chain[index-1];
-            if(currentBlock.hash !== currentBlock.calculateHash()){
+            const previousBlock = this.chain[index - 1];
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
                 return false
             }
 
-            if(currentBlock.previousHash !=previousBlock.hash){
+            if (currentBlock.previousHash != previousBlock.hash) {
                 return false
             }
             return true
@@ -45,9 +57,9 @@ class BlockChain {
 }
 
 const samuCoin = new BlockChain()
-samuCoin.addBlock(new Block(1, '05/05/2022', {amount: 10}))
-samuCoin.addBlock(new Block(2, '15/05/2022', {amount: 9}))
 
-console.log(JSON.stringify(samuCoin, null, 2))
+console.log('Mining block 1...');
+samuCoin.addBlock(new Block(1, '05/05/2022', { amount: 10 }))
 
-console.log('isChainVAlid', samuCoin.isChainVAlid());
+console.log('Mining block 2...');
+samuCoin.addBlock(new Block(2, '15/05/2022', { amount: 9 }))
